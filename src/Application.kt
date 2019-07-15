a9bcb165755e95d13d4116f4fe16b5375fa9b896
Christine R. Nagadya (ThoughtWorks) <ncritah@gmail.com>
2019-07-15 14:59:31 +0200

    Setting up

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        //get dsl takes the path string as first param and body lambda as its second
+        get("/") {
+            call.respondText { "hello world" }
+        }
+
+    }
+}
+
